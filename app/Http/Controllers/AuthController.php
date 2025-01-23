923abceb22ea058455329141a387577148501a8b
Rafael Costa <rafael.costa1992@live.com>
2025-01-23 16:44:45 -0300

    Validação de Dados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,19 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo 'Login Submit';
+        //Validação de Formulario
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+
+            ]);
+
+            //Get user input
+            $username = $request->input('text_username');
+            $password = $request->input('text_password');
+
+            echo "OK!";
     }
 
     public function logout()
